0db1370951cfd046585d606576d036054f619eeb
Manot Luijiu <manot.luijiu@gmail.com>
2021-06-04 10:16:17 +0700

    Using path to join absolute path

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware - Add Product add');
-  res.send(
-    '<form action="/admin/add-product" method="post">\
-      <input type="text" name="title">\
-      <button type="submit">Submit</button>\
-      </input>\
-      </form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
